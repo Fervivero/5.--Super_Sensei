5b13baaf667168e697df7c6855e6b175494a2027
Fernanda Vives <mfernandavives@gmail.com>
2023-07-10 21:57:04 -0400

    Super_Ninja

---
+++ super_ninja.js
@@ -0,0 +1,26 @@
+class Ninja {
+    constructor(name) {
+        this.name = name;
+        this.health = 200;
+        this.speed = 10;
+        this.strength = 10;
+    }
+
+    speakWisdom() {
+        console.log("Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses.");
+    }
+
+    showStats() {
+        console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`);
+    }
+
+    drinkSake() {
+        this.health += 10;
+        console.log(`Mi salud aumentó a ${this.health}`);
+    }
+}
+
+const ninja = new Ninja("Karate Kid");
+ninja.showStats();
+ninja.drinkSake();
+ninja.speakWisdom();
